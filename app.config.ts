fbb5fedce1a49e28a732a95cd698eb4e494e4b1a
Sandhya Chellangi <sandhyachellangi@Mac.ultrahub>
2026-01-21 18:27:27 +0000

    Bump version to 3 in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name: "SmashTracker",
     slug: "smashtracker",
-    version: "1.0.0",
+    version: "2.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -26,7 +26,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         "backgroundColor": "#ffffff"
       },
       "package": "com.gk.smashtracker",
-      "versionCode": 1,
+      "versionCode": 3,
       // Dynamic Google Services File
       "googleServicesFile": process.env.GOOGLE_SERVICES_FILE || "./google-services.json",
       "edgeToEdgeEnabled": true,
